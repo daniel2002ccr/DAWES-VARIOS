b391e6a1ab8ef134f7cdf55a48fcd246fb30b9a1
daniel2002ccr <115981812+daniel2002ccr@users.noreply.github.com>
2023-10-20 14:02:13 +0200

    Add files via upload

---
+++ PrimerServlet/src/main/java/com/daniel/controladores/MiServlet.java
@@ -0,0 +1,37 @@
+package com.daniel.controladores;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class MiServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+    public MiServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		
+		PrintWriter out = response.getWriter();
+		
+		out.print("<html> <body>");
+		out.print("<h1>Hola Mundo</h1>");
+		out.print("</body> </html>");
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ PrimerServlet/src/main/java/com/daniel/controladores/SegundoServlet.java
@@ -0,0 +1,34 @@
+package com.daniel.controladores;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import com.daniel.java.GeneradorNumeroAleatorio;
+
+@WebServlet("/SegundoServicio")
+public class SegundoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public SegundoServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int numAleatorio = GeneradorNumeroAleatorio.numeroAleatorio();
+		response.getWriter().append("Numero: " + numAleatorio);
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
