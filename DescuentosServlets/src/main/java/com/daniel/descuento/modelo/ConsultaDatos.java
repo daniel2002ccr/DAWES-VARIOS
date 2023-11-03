35a791ef626184229175a4854db8167c1d4faa7f
alumnofp <alumnofp@FPB06.CALASANZSALAMANCA.local>
2023-11-03 12:37:08 +0100

    Descuentos servlets

---
+++ DescuentosServlets/src/main/java/com/daniel/descuento/modelo/ConsultaDatos.java
@@ -0,0 +1,13 @@
+package com.daniel.descuento.modelo;
+
+import java.io.File;
+
+public class ConsultaDatos {
+
+	public Integer consultaEdad(String nombre) {
+		
+		File f = new File("")
+		
+		return null;
+	}
+}
--- ReservaSalasMaven/src/main/java/com/daniel/reserva/negocio/ReservaSalas.java
+++ ReservaSalasMaven/src/main/java/com/daniel/reserva/negocio/ReservaSalas.java
@@ -4,18 +4,17 @@ package com.daniel.reserva.negocio;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Random;
 
 public class ReservaSalas {
 
 	private boolean esFechaValida(String fechaHora) throws ParseException {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
-        sdf.setLenient(false);
-     
-            Date fecha = sdf.parse(fechaHora);
-            Date now = new Date(); 
-            return !fecha.before(now); 
-       
+	    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+	    Date fecha = sdf.parse(fechaHora);
+	    Date now = new Date();
+	    return !fecha.before(now);
 	}
+	
 	private boolean esHoraEnPunto(String fechaHora) {
         String[] fecha = fechaHora.split(" ");
         String hora = fecha[1]; 
@@ -24,4 +23,10 @@ public class ReservaSalas {
         return minutos == 0; 
     }
 
+	private int generarCodigoConfirmacion() {
+	    Random random = new Random();
+	    return random.nextInt(300); 
+	     
+	}
+	
 }
