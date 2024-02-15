4349e21e2bc7701894018586dd1259b03233fd7c
alumnofp <alumnofp@FPB06.CALASANZSALAMANCA.local>
2024-02-15 14:05:33 +0100

    No se

--- Colegio/src/main/java/com/daniel/colegio/controlador/rest/AlumnosRestController.java
+++ Colegio/src/main/java/com/daniel/colegio/controlador/rest/AlumnosRestController.java
@@ -1,14 +1,21 @@
 package com.daniel.colegio.controlador.rest;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.daniel.colegio.dtos.AlumnoDTO;
 import com.daniel.colegio.entities.AlumnoEntity;
 import com.daniel.colegio.repositories.AlumnosRepository;
 
@@ -18,21 +25,26 @@ public class AlumnosRestController {
 
 	@Autowired
 	private AlumnosRepository alumnosRepository;
-	
-	
+
 	@PostMapping("/alumnos")
-	public ResponseEntity insertarAlumno(@RequestBody AlumnoEntity alumno){
-		
+	public ResponseEntity insertarAlumno(@RequestBody AlumnoEntity alumno) {
+
 		alumnosRepository.save(alumno);
-		
+
 		return new ResponseEntity<>("Alumno insertado con éxito", HttpStatus.OK);
 	}
+
 	@PutMapping("/alumnos")
-	public ResponseEntity actualizarAlumno(@RequestBody AlumnoEntity alumno){
-		
+	public ResponseEntity actualizarAlumno(@RequestBody AlumnoEntity alumno) {
+
 		alumnosRepository.save(alumno);
-		
+
 		return new ResponseEntity<>("Alumno aktualisado con éxito", HttpStatus.OK);
 	}
-	
+
+	@GetMapping(value = "/alumnos/{id}")
+	public AlumnoEntity obtenerAlumnoPorID(@PathVariable("id") Integer id) {
+		return alumnosRepository.findById(id).get();
+	}
+
 }
--- Colegio/src/main/java/com/daniel/colegio/entities/AlumnoEntity.java
+++ Colegio/src/main/java/com/daniel/colegio/entities/AlumnoEntity.java
@@ -1,13 +1,14 @@
 package com.daniel.colegio.entities;
 
-import java.util.List;
+
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -25,6 +26,7 @@ public class AlumnoEntity {
 //	private Integer idMunicipio;
 
 	@ManyToOne
+	@JsonBackReference
 	@JoinColumn(name = "id_municipio")
 	private MunicipioEntity municipio;
 	@Column(name = "familia_Numerosa")
